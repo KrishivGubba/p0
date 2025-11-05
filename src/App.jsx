2ecd9aad5b1a1ef5b721df5026e9c83e94f95925
Krishiv Gubba <krishivgubba@Krishivs-MacBook-Pro.local>
2025-11-05 11:58:59 -0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello Badgers 🦡</h1>
+      <p>This is my CS 571 initial publish proof of concept!</p>
+    </div>
+  );
+}
+
+export default App;
